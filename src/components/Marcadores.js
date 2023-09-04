a4a314a17b188f828b04525f25039d649b4c4e9e
mauriander <andermattenmauricio@gmail.com>
2023-09-03 21:47:00 -0300

    input nombre

--- src/components/Marcadores.js
+++ src/components/Marcadores.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Marcadores(props){
-return (
+function Marcadores(props) {
+  //let nombre="Mauricio";
+  const [nombre, setNombre] = useState("Mauri");
+  const guardar = (name) => {
+    setNombre(name);
+  };
+
+  return (
     <div>
-{props.children}
+      {props.children}
+      <input
+        type="text"
+        value={nombre}
+        onChange={(e) => guardar(e.target.value)}
+      ></input>
+      <button onClick={() => guardar(nombre)}>Guardar</button>
 
-
-<p>Input de resultados</p>
-
+      <p>Input de resultados de {nombre}</p>
     </div>
-)
-
+  );
 }
 
 export default Marcadores;
