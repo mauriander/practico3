339ae5b9776eac4b923cb3cb0ab1b9f62592cb04
mauriander <andermattenmauricio@gmail.com>
2023-09-10 22:50:15 -0300

    AGREGO boton reiniciar y cambio guardar por inicar

--- src/components/Marcadores.js
+++ src/components/Marcadores.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
 
+import TurnoJugador from './TurnoJugador';
+import TurnoPc from './TurnoPc'
+
 function Marcadores(props) {
-  //let nombre="Mauricio";
+
   const [nombre, setNombre] = useState("Mauri");
-  const guardar = (name) => {
+  const [puntajeUsuario, setPuntajeUsuario] = useState(0);
+  const [puntajePC, setPuntajePC] = useState(0);
+
+ 
+   const actualizarPuntajes = (resultado) => {
+    if (resultado === "Usted Gana") {
+      setPuntajeUsuario(puntajeUsuario + 1);
+    } else if (resultado === "Tu pierdes, vuelve a intentarlo") {
+      setPuntajePC(puntajePC + 1);
+    }
+  };
+
+ const validarNombre = (nombre) => {
+    return nombre.trim().length > 0;
+  };
+
+ const reiniciar = () => {  window.location.reload();}
+  const guardar = (name) => {    
     setNombre(name);
   };
+
  const tab = <>&nbsp;&nbsp;&nbsp;&nbsp;</>;
   return (
     <div>
@@ -15,10 +36,10 @@ function Marcadores(props) {
         value={nombre}
         onChange={(e) => guardar(e.target.value)}
       ></input>
-      <button onClick={() => guardar(nombre)}>Guardar</button>
-
+      <button onClick={() => guardar(nombre)}>Iniciar</button>
+<button onClick={() => reiniciar()}>Reiniciar</button>
       <p><strong>{nombre}</strong>  {tab} <span><strong>PC</strong></span> </p>
-        <p>Puntaje {nombre}  {tab}<span>Puntaje pc</span></p>
+        <p>Puntaje {nombre} {puntajeUsuario} {tab}<span>Puntaje pc{puntajePC} </span></p>
 <div><span></span></div>
       
     </div>
--- src/components/TurnoPc.js
+++ src/components/TurnoPc.js
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
 import jugadaPC from '../components/TurnoJugador.js'
-function TurnoPC(props){
+const TurnoPC = ({jugada})=>{
 
 
 
 
 return (
     <div>
-{props.children}
-
 
 <p>Input de resultados</p>
  <div>
+  <p>MUESTRO JUGADA COMPONENTE PC</p>
  <div className="item">
       <button> <img 
         className='user_img'
-        src={jugadaPC}
+        src={jugada}
         alt='user'></img>
      </button>
 </div>
      </div>
     </div>
-)
+);
 
-}
+};
 
 export default TurnoPC;
